0eaa4b13cdd05edcf235eb1eae2615ca35d0b122
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-06 21:39:01 -0800

    multiple questions working with game over screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
 
-    var trivia = [{ q: "What color is the sky?", 1: "yellow", 2: "blue", 3: "orange", 4: "green", ans: "2" }];
+    var trivia = [{ q: "What color is the sky?", 1: "yellow", 2: "blue", 3: "orange", 4: "green", ans: "2" },
+                  { q: "What color is the ocean?", 1: "yellow", 2: "red", 3: "orange", 4: "blue", ans: "4" }];
     var index = 0;
     var guess;
     var wins = 0;
 
-    $(".section_1").text(trivia[index].q);
+    game();
 
+   
 
-    for (var i = 1; i < 5; i++) {
-
-        $(".section_2").append(
-            $("<button/>", {
-                text: trivia[index][i],
-                id: "btn_" + i,
-                value: i,
-                click: function () {
-                    guess = $(this).val();
-                    checkCorrect(guess);
-
-                }
-            }), "<br/>"
-        )
+    function answerButtons() {
+        for (var i = 1; i < 5; i++) {
+            $(".section_2").append(
+                $("<button/>", {
+                    text: trivia[index][i],
+                    id: "btn_" + i,
+                    value: i,
+                    click: function () {
+                        guess = $(this).val();
+                        checkCorrect(guess);
+                    }
+                }), "<br/>"
+            )
+        }
     }
-
+    
 
     function checkCorrect(g) {
         var answer = trivia[index].ans;
@@ -36,12 +38,22 @@ $(document).ready(function () {
         } else {
             $(".section_1").html("Nope!!");
             $(".section_2").html("The correct answer was " + text);
-        }
-
-
-
+        }    
+        index++;
+        setTimeout( function () {
+            game();
+        } , 3000 )
     }
 
-
+    function game(){
+        if (index < trivia.length){
+            $(".section_1").text(trivia[index].q);  
+            answerButtons();
+        } else {
+            $(".section_1").html("You won " + wins + " games out of " + trivia.length);
+            $(".section_2").html("");  
+        }
+        
+    }
 
 });
